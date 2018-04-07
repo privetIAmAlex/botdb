98dfb779fc469088131a2b4965d452634dd8e774
Alexander <31483533+privetIAmAlex@users.noreply.github.com>
2018-04-08 00:11:29 +0800

    New bot.

--- bot.py
+++ bot.py
@@ -1,50 +1,22 @@
-import telebot
+from telebot import TeleBot
 from worker import Worker
-import time
 
-bot = telebot.TeleBot("492864827:AAFc_KDXUf4-06pZqstFv6HaPO5m5LaruvE")
-worker = Worker(bot)
+bot = TeleBot("492864827:AAFc_KDXUf4-06pZqstFv6HaPO5m5LaruvE")
+_worker = Worker(bot)
 
-@bot.message_handler(content_types=["audio", "document", "sticker", "video", "contact"])
-def other_type_handler(message):
-	worker.Counter(message.from_user.id)
+@bot.message_handler(content_types=["new_chat_member"])
+def handle_new_member(message):
+    _worker.HelloUser(message.chat.id, message.from_user.first_name)
 
-@bot.message_handler(content_types=["photo"])
-def handle_photo(message):
-    worker.Counter(message.from_user.id)
-    if message.caption != None and worker.FindBadWord(message.caption):
-        try:
-            worker.BlockUser(message.from_user.id, message.message_id, message.chat.id, message.from_user.first_name)
-        except:
-            bot.send_message(message.chat.id, "Так-с, материмся?👮‍♀️", reply_to_message_id=message.message_id)
-
-@bot.message_handler(content_types=["new_chat_members"])
-def new_members_handler(message):
-    if message.from_user.id != 497551952:
-        worker.HelloUser(message)
+@bot.message_handler(content_types=["photo", "audio", "document", "sticker", "video", "contact"])
+def handle_other_types(message):
+    _worker.Counter(message.from_user.id)
 
 @bot.message_handler(content_types=["text"])
 def handle_message(message):
     if message.chat.id == 497551952:
-        day = time.strftime("%w")
-        letter = worker.GetStat(day)
-        if message.text == "send_stat" and int(day) == 0:
-            bot.send_message(-1001137097313, letter, parse_mode="HTML")
-        elif message.text == "send_stat_me":
-            bot.send_message(497551952, letter, parse_mode="HTML")
-            bot.send_message(497551952, day)
-            bot.send_message(497551952, str(worker.mess))            
-        elif message.text == "unmute":
-            bot.restrict_chat_member(-1001137097313, 497551952, until_date=time.time()+35)
-        elif message.text == "send_time":
-            bot.send_message(497551952, str(time.strftime("%H:%M")))
+        _worker.AdminPanel(message.text)
         return
-    worker.Counter(message.from_user.id)
-    if worker.FindBadWord(message.text):
-        try:
-            worker.ReplaceBadWord(message)
-        except Exception as ex:
-            #bot.send_message(message.chat.id, "Так-с, материмся?👮‍♀️", reply_to_message_id=message.message_id)
-            print(str(ex))
-    
+    _worker.FindBadWord(message.chat.id, message.from_user.first_name, message.text)
+
 bot.polling(none_stop=True)
--- worker.py
+++ worker.py
@@ -1,8 +1,13 @@
-import random
-from peewee import *
-import psycopg2
+from random import choice
+from peewee import PostgresqlDatabase, Model, IntegerField, DoesNotExist
+from re import findall
 import time
-import threading
+phrases = [
+        "Добро пожаловать, {0}!☺️\n\nУ нас культурный чат! Здесь нельзя материться и оскорблять других участников.🚫\nВсе правила ты можешь посмотреть в закреплённом сверху сообщении👆",
+        "Приветствую тебя, {0}!✋️\n\nУ нас культурный чат! Здесь нельзя материться и оскорблять других участников.🚫\nВсе правила ты можешь посмотреть в закреплённом сверху сообщении👆",
+        "Какие люди! Привет, {0}!😎\n\nПожалуйста, соблюдай правила группы (их ты можешь посмотреть в закреплённом сверху сообщении👆), а то забаню. Шутка🤣",
+        "Привет, {0}!🤓\n\nУ нас очень дружелюбный чат, так что не бойся писать, мы не кусаемся!😄 \n\nПравила ты можешь посмотреть в закреплённом сверху сообщении👆"
+    ]
 
 db = PostgresqlDatabase(
 	database="d8f1mjaq4lsqmk",
@@ -13,87 +18,45 @@ db = PostgresqlDatabase(
 )
 
 class Person(Model):
-    user_id = IntegerField()
+    id = IntegerField()
     count_messages = IntegerField()
-    ban_id = IntegerField(default=0)
-    class Meta:
-        database = db
-
-class Record(Model):
-    total_counts = IntegerField(default=0)
     class Meta:
         database = db
 
 Person.create_table()
-Record.create_table()
 
-rc = Record.create(total_counts=1605)
-rc.save()
+bad_words = []
 
-class Worker:
-    mess = ""
-    BOT = None
-    bad_words = []
-    phrases = [
-        "Добро пожаловать, {0}!☺️\n\nУ нас культурный чат! Здесь нельзя материться и оскорблять других участников.🚫\nВсе правила ты можешь посмотреть в закреплённом сверху сообщении👆\n\nДавай познакомимся? Расскажи немного о себе!🤗",
-        "Приветствую тебя, {0}!✋️\n\nУ нас культурный чат! Здесь нельзя материться и оскорблять других участников.🚫\nВсе правила ты можешь посмотреть в закреплённом сверху сообщении👆\n\nРасскажешь что-нибудь о себе?😊",
-        "Какие люди! Привет, {0}!😎\n\nПожалуйста, соблюдай правила группы, а то забаню. Шутка🤣\nВсе правила ты можешь посмотреть в закреплённом сверху сообщении👆\n\nДавай познакомимся? Расскажи что-нибудь о себе🤗",
-        "Привет, {0}!🤓\n\nУ нас очень дружелюбный чат, так что не бойся писать, мы не кусаемся!😄 Только не матерись, пожалуйста😉\n\nВсе правила ты можешь посмотреть в закреплённом сверху сообщении👆"
-    ]
-
-    def __init__(self, _bot):
-        self.BOT = _bot
-        with open('badwords.txt', 'r', encoding='cp1251') as f:  
-            content = f.readlines()
+class Worker():
+    def __init__(self, bot):
+        self._bot = bot
+        with open('badwords.txt', 'r', encoding='cp1251') as file:
+            content = file.readlines()
             for word in content:
-                self.bad_words.append(word[:-1])
+                bad_words.append(word[:-1])
 
-    def HelloUser(self, _message):
-        user_name = _message.new_chat_member.first_name
-        self.BOT.send_message(_message.chat.id, random.choice(self.phrases).format(user_name))
+    def HelloUser(self, chat_id, user_name):
+        self._bot.send_message(chat_id, choice(phrases).format(user_name))
 
     def Counter(self, user_id):
         try:
-            a = Person.get(Person.user_id == user_id)
-            a.count_messages += 1
-            a.save()
+            p = Person.get(id=user_id)
+            p.count_messages += 1
+            p.save()
         except DoesNotExist:
-            Person.create(user_id=user_id, count_messages = 1, ban_id=0)
+            Person.create(id=user_id, count_messages=1).save()
 
     def GetAsterics(self, x):
-        return "*" * x
+        return "*" * (x - 2)
 
-    def ReplaceBadWord(self, message):
-        text = message.text
-        for word in self.bad_words:
-            if word in text:
-                text = text.replace(word, self.GetAsterics(len(word.strip())))
-        self.BOT.send_message(message.chat.id, f"<b>{message.from_user.first_name}</b>: {text}", parse_mode="HTML")
-        self.BOT.delete_message(message.chat.id, message.message_id)
+    def FindBadWord(self, chat_id, first_name, text):
+        text_array = findall(r"[\w']+", text.lower())
+        for word in text_array:
+            if word in bad_words:
+                new_word = word.replace(word[:-2], self.GetAsterics(len(word)))
+                text = text.replace(word, new_word)
+        self._bot.send_message(chat_id, f"<b>{first_name}:</b> {text}")
 
-    def FindBadWord(self, text):
-        text = text.lower()
-        for word in self.bad_words:
-            if word in text:
-                return True
-        return False
-
-    def BlockUser(self, user_id, message_id, chat_id, first_name):
-        a = Person.get(Person.user_id == user_id)
-        a.ban_id = a.ban_id + 1
-        a.save()
-        now = time.time()
-        if a.ban_id >= 3:
-            self.BOT.restrict_chat_member(chat_id, user_id, until_date=now+3600)
-            self.BOT.send_message(chat_id, "<b>{} заблокирован(а) на 1 час</b>\n\n{}, у нас нельзя материться!\nВ следующий раз наказание будет строже!".format(first_name, first_name), parse_mode="html")
-        elif a.ban_id == 2:
-            self.BOT.restrict_chat_member(chat_id, user_id, until_date=now+300)
-            self.BOT.send_message(chat_id, "<b>{} заблокирован(а) на 5 минут</b>\n\n{}, у нас нельзя материться!\nВ следующий раз наказание будет строже!".format(first_name, first_name), parse_mode="html")
-        else:
-            self.BOT.restrict_chat_member(chat_id, user_id, until_date=now+60)
-            self.BOT.send_message(chat_id, "<b>{} заблокирован(а) на 1 минуту</b>\n\n{}, у нас нельзя материться!\nВ следующий раз наказание будет строже!".format(first_name, first_name), parse_mode="html")
-        self.BOT.delete_message(chat_id, message_id)
-        
     def CurrentWord(self, number):                  
         iy = ['11', '12', '13', '14', '5', '6', '7', '8', '9', '0']
         if number.endswith('1'):
@@ -105,12 +68,14 @@ class Worker:
                 if iy[i] in number:
                     return "сообщений"
 
-    def GetStat(self, _day):
+    def AdminPanel(self, command):
+        print("Зашёл в метод")
         stat = ""
         iter = 0
         for one in Person.select().order_by(Person.count_messages.desc()).limit(10):
+            print("В цикле")
             try:
-                _user = self.BOT.get_chat_member(-1001137097313, one.user_id)
+                _user = self._bot.get_chat_member(-1001137097313, one.id)
                 name = "@" + _user.user.username if _user.user.username != None else _user.user.first_name
                 if iter == 0: 
                     stat += f"🥇{name} - {one.count_messages}\n"
@@ -126,26 +91,15 @@ class Worker:
             except Exception:
                 stat += f"~outgoing - {one.count_messages}\n"
                 iter += 1
+        print("Вышел из цикла")
         total = 0
         for i in Person.select():
             total += i.count_messages
-
-        insert = ""
-        for rec in Record.select():
-            self.mess = f"total = {str(total)}\nrec.total_counts = {str(rec.total_counts)}"
-            if total > rec.total_counts:
-                insert = "Мы поставили новый рекорд!🎉"
-                a = Record.create(total_counts=total)
-                rec.delete_instance()
-                a.save()                    
-            else:
-                res = rec.total_counts - total
-                insert = f"Ещё бы <b>{res}</b> {self.CurrentWord(str(res))} и мы побили бы прошлый рекорд😌"
-        if _day == 0:
-            for per in Person.select():
-                per.count_messages = 0
-                per.save()
-        letter = "Вот и подошла к концу ещё одна неделя! И вот вам немного статистики:\n\n<i>Самые активные участники:</i>\n{}\nА всего было напечатано <b>{}</b> {}!\n{}\n\nУдачи в наступающей неделе!😉".format(stat, total, self.CurrentWord(str(total)), insert)
-        return letter
-
-        ##
+        letter = "Вот и подошла к концу ещё одна неделя! И вот вам немного статистики:\n\n<i>Самые активные участники:</i>\n{}\nА всего было напечатано <b>{}</b> {}!\n\nУдачи в наступающей неделе!😉".format(stat, total, self.CurrentWord(str(total)))
+        
+        if command == "send_stat_me":
+            print("Принял команду")
+            self._bot.send_message(497551952, letter, parse_mode="HTML")
+        elif command == "send_stat":
+            if time.strftime("%w") == 0:
+                self._bot.send_message(-1001137097313, letter, parse_mode="HTML")            
